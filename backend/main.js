618cb470f02729b8325c259003e2de7c8614fbcf
vitlio <gorshkov.vitalik@yandex.ru>
2022-08-04 22:54:47 +0300

    delete unnecessary comment

--- backend/main.js
+++ backend/main.js
@@ -7,19 +7,6 @@ const app = express()
 const server = new Websocket.Server({ port: 8080 })
 const PORT = process.env.PORT || 3000
 
-// server.on('connection', ws => {
-//     console.log('connect')
-    // ws.on('message', message =>{
-    //     server.clients.forEach(client => {
-    //         console.log(client)
-    //         if(client.readyState === Websocket.OPEN){
-    //             client.send(message.toString())
-    //         }
-    //     })
-    // })
-//     ws.send('Привет!')
-// })
-
 app.use('/', express.static(path.resolve(__dirname, '../frontendClient/')))
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontendClient/index.html'))
